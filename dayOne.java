5eb645dcdc6d59c25407de949a44fd32c99295b9
Jack C <43814809+meatball084@users.noreply.github.com>
2021-12-01 12:35:56 -0600

    Create dayOne.java

---
+++ dayOne.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.lang.*;
+import java.io.*;
+import java.sql.Array;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        Scanner s = new Scanner(new File("src/com/company/data.dat"));
+        int[] mem = new int[2];
+        mem[0] = s.nextInt();
+        mem[1] = s.nextInt();
+        int i = 0, x = 0;
+        if(mem[0] < mem[1]) x++;
+        while (s.hasNextInt()){
+            mem[0] = mem[1];
+            mem[1] = s.nextInt();
+            if(mem[0] < mem[1]){
+                x++;
+            }
+            System.out.println("(" + mem[0] + ", " + mem[1] + ")" + ": " + x);
+        }
+    }
+}
